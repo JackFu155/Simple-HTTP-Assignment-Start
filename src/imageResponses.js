79a9e6f25ca34ece27318e1223fefca194009adc
Jack Furmanek <jackfu155@gmail.com>
2020-01-30 10:57:03 -0500

    First Commit

---
+++ src/imageResponses.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const spongegar = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getSpongegar = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(spongegar);
+  response.end();
+};
+
+module.exports.spongegar = spongegar;
+module.exports.getSpongegar = getSpongegar;
